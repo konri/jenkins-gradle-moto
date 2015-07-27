e12e9600fe32893430ecdfc71081a24da6194199
konrad hopek <PKB734@motorolasolutions.com>
2015-07-27 14:45:38 +0200

    newest

---
+++ app/src/test/java/jenkins_grovle_test/com/jenkins_android_grovle_testing/MainActivityTest.java
@@ -0,0 +1,24 @@
+package jenkins_grovle_test.com.jenkins_android_grovle_testing;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by PKB734 on 27-Jul-2015.
+ */
+public class MainActivityTest extends TestCase {
+    @Test
+    public void testFake1() throws Exception {
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void testFake2() throws Exception {
+        Assert.assertFalse(false);
+    }
+    @Test
+    public void testFake3() throws Exception {
+        Assert.assertFalse("Should be false", false);
+    }
+}
